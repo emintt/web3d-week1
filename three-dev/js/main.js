5222301d67acc2cddabe3c534aa554ea356c94bd
liem nguyen <elminguyen@gmail.com>
2024-10-24 23:36:33 +0300

    week 1

--- three-dev/js/main.js
+++ three-dev/js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
-let container, camera, scene, renderer, cube, controls;
+let container, camera, scene, renderer, ball1, ball2, hat, snowman, controls;
 
 init();
 
@@ -18,21 +18,10 @@ function init() {
   // create orbit controls
   controls = new OrbitControls( camera, renderer.domElement );
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-  cube = new THREE.Mesh( geometry, material );
-
-  // object transformation
-  cube.position.x = 0;
-  cube.scale.set(2,2,2);
-  cube.rotation.y = Math.PI / 4;
-  scene.add( cube );
-
-  const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
+  const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
   scene.add(directionalLight);
 
-  const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
+  const ambientLight = new THREE.AmbientLight( 0x000000 ); 
   scene.add( ambientLight );
 
   // Create a PointLight 
@@ -40,9 +29,29 @@ function init() {
   camera.add( pointLight );
   scene.add( camera );
 
+  // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const material = new THREE.MeshPhongMaterial( { color: 0x248f6f } );
+
+  snowman = new THREE.Group();
+  scene.add(snowman);
+  ball1 = new THREE.Mesh( new THREE.SphereGeometry( 3, 32, 16 ), material );
+  // object transformation
+  ball1.position.x = 0;
+  // ball1.scale.set(2,2,2);
+  ball1.rotation.y = Math.PI / 4;
+  snowman.add( ball1 );
+
+  ball2 = new THREE.Mesh( new THREE.SphereGeometry( 2, 32, 16 ), material );
+  ball2.position.y = 4;
+  snowman.add( ball2 );
+
+  hat = new THREE.Mesh( new THREE.CylinderGeometry( 1, 3, 2, 32, 1 ), material );
+  hat.position.y = 6;
+  snowman.add( hat );
 
   //controls.update() must be called after any manual changes to the camera's transform
-  camera.position.set(2,2,5);
+  camera.position.set(2,2,15);
   controls.update();
 
   const axesHelper = new THREE.AxesHelper( 5 );
@@ -56,8 +65,8 @@ function init() {
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	snowman.rotation.x += 0.01;
+	snowman.rotation.z += 0.01;
 
   // required if controls.enableDamping or controls.autoRotate are set to true
 	controls.update();
